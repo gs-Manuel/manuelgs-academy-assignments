8f43f5dc0feb5285fb5c6b4ec24d9bcf436a828a
Manuel <gs_manuel@outlook.com>
2022-06-28 12:45:58 +0200

    Quick error solve

--- Exercise_2/src/test/java/com/github/gsManuel/Exercise_2/controllers/operations/AddTest.java
+++ Exercise_2/src/test/java/com/github/gsManuel/Exercise_2/controllers/operations/AddTest.java
@@ -3,14 +3,14 @@ package com.github.gsManuel.Exercise_2.controllers.operations;
 import com.github.gsManuel.Exercise_2.controllers.Operations;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.AssertTrue.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddTest implements Operations {
     @Test
     public void givenNegativeNumberswhenAddthenGoodres() {
-        assertTrue(Operations.add(1, 2), 3);
-        assertTrue(Operations.add(-1, -8), -9);
-        assertTrue(Operations.add(0, 0), 0);
-        assertTrue(Operations.add(2.5, 5.7), 8.2);
+        assertEquals(Operations.add(1, 2), 3.0);
+        assertEquals(Operations.add(-1, -8), -9);
+        assertEquals(Operations.add(0, 0), 0);
+        assertEquals(Operations.add(2.5, 5.7), 8.2);
     }
 }
